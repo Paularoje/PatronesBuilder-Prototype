cb9f777f5acc38b20768d9010929cdf3f2a90d57
Paula Rodriguez <mapaurodriguez@hotmail.com>
2022-02-16 22:07:43 -0500

    Subiendo los archivos .java del proyecto

---
+++ Builder/src/builder/Director.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package builder;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Director
+{
+    public Director() {
+    }
+    // --------------------------
+     public void construir( BuilderCoche builder )
+    {
+        builder.crearNuevoCoche();
+        builder.construirMotor();
+        builder.construirCarroceria();
+        builder.construirElevalunas();
+        builder.construirAireAcond();
+    }
+}
